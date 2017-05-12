7ad6ef45ebb9fb04c9735fe799aa7763cb2b9651
TsubasaK111 <Tsubasa.K111@gmail.com>
2017-05-12 20:06:24 +0900

    feat: update output to verbose new standard

--- main.go
+++ main.go
@@ -6,11 +6,17 @@ import (
 )
 
 func main() {
+  // このコードは引数と標準出力を用いたサンプルコードです。
+  // このコードは好きなように編集・削除してもらって構いません。
+  // ---
+  // This is a sample code to use arguments and outputs.
+  // Edit and remove this code as you like.
+
 	run(os.Args[1:])
 }
 
 func run(args []string) {
-	for _, v := range args {
-		fmt.Println(v)
+	for i, v := range args {
+		fmt.Printf("argv[%d]: %s\n", i, v)
 	}
 }
